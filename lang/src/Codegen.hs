18e0198d4fb5c9f1a8ebee5026bfe7b9c2f82c77
Isaac Elliott <isaace71295@gmail.com>
2019-06-30 14:28:22 +1000

    improved ll conversion

--- lang/src/Codegen.hs
+++ lang/src/Codegen.hs
@@ -2,44 +2,40 @@
 module Codegen where
 
 import Data.Foldable (for_)
+import Data.Word (Word64)
 import IR (IR)
 import qualified IR
 
 import Syntax
 
-cg_expr :: (a -> IR IR.Val) -> C_Expr a -> IR IR.Val
-cg_expr var = go (error "not in function")
+cg_expr :: (String -> IR IR.Val) -> (a -> IR IR.Val) -> LL a -> IR IR.Val
+cg_expr name var = go (error "not in function")
   where
     go env ex =
       case ex of
-        C_Var a -> var a
-        C_Bound -> IR.R <$> IR.load0 (IR.R IR.sp)
-        C_Env n -> seq env $ IR.R <$> IR.load (IR.C $ IR.W64 n) (IR.R env)
-        C_UInt64 a -> pure $ IR.C (IR.W64 a)
-        C_Add a b -> do
+        LVar a -> var a
+        LName a -> name a
+        LUInt64 a -> pure $ IR.C (IR.W64 a)
+        LProduct as -> do
+          let las = length as
+          loc <- IR.alloc (IR.C . IR.W64 $ fromIntegral las)
+          for_ (zip [0::Word64 ..] as) $ \(n, a) -> do
+            a' <- go env a
+            IR.store a' (IR.C $ IR.W64 n) (IR.R loc)
+          pure $ IR.R loc
+        LProj n a -> do
+          a' <- go env a
+          IR.R <$> IR.load (IR.C $ IR.W64 n) a'
+        LBin o a b -> do
           va <- go env a
           vb <- go env b
-          IR.R <$> IR.add va vb
-        C_Mult a b -> do
-          va <- go env a
-          vb <- go env b
-          IR.R <$> IR.mul va vb
-        C_Lam e body -> do
-          let el = length e
-          envp <- do
-            p <- IR.alloc (IR.C . IR.W64 $ fromIntegral el)
-            for_ (zip [0..] e) $ \(ix, val) -> do
-              v <- go env val
-              IR.store (IR.C $ IR.W64 ix) v (IR.R p)
-            pure p
-          res <- go envp body
-          IR.mov res IR.rv
-          IR.R IR.rv <$ IR.ret
-        C_App f x -> do
+          case o of
+            Add -> IR.R <$> IR.add va vb
+            Mult -> IR.R <$> IR.mul va vb
+        LApp f e x -> do
           vf <- go env f
+          ve <- go env e
           vx <- go env x
-          loc <- do
-            envp <- IR.load0 vf
-            IR.add (IR.R envp) (IR.C $ IR.W64 1)
+          IR.push ve
           IR.push vx
-          IR.R IR.rv <$ IR.icall (IR.R loc)
+          IR.R IR.rv <$ IR.icall vf
--- lang/src/Syntax.hs
+++ lang/src/Syntax.hs
@@ -1,79 +1,97 @@
 {-# language DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 {-# language StandaloneDeriving #-}
+{-# language FlexibleContexts, RecursiveDo, ScopedTypeVariables #-}
 {-# language TemplateHaskell #-}
 module Syntax where
 
-import Bound.Scope (Scope, fromScope)
+import Bound.Scope.Simple (Scope, fromScope, toScope)
 import Bound.TH (makeBound)
-import Bound.Var (unvar)
-import Data.Deriving (deriveShow1)
-import Data.Functor.Classes (showsPrec1)
-import Data.List (union)
+import Bound.Var (Var(..), unvar)
+import Control.Monad.Fix (MonadFix)
+import Control.Monad.State (MonadState, evalStateT, get, put)
+import Control.Monad.Writer (MonadWriter, runWriter, tell)
+import Data.Deriving (deriveEq1, deriveShow1)
+import Data.Functor.Classes (eq1, showsPrec1)
+import Data.List (elemIndex, union)
 import Data.Word (Word64)
 
-import qualified Data.Map as Map
+data Op = Add | Mult deriving (Eq, Show)
 
-data Expr a
+data Exp a
   = Var a
   | UInt64 !Word64
-  | Add (Expr a) (Expr a)
-  | Mult (Expr a) (Expr a)
-  | Lam (Scope () Expr a)
-  | App (Expr a) (Expr a)
+  | Bin Op (Exp a) (Exp a)
+  | Lam (Scope () Exp a)
+  | App (Exp a) (Exp a)
   deriving (Functor, Foldable, Traversable)
-makeBound ''Expr
-deriveShow1 ''Expr
-instance Show a => Show (Expr a) where; showsPrec = showsPrec1
+makeBound ''Exp
+deriveEq1 ''Exp
+deriveShow1 ''Exp
+instance Eq a => Eq (Exp a) where; (==) = eq1
+instance Show a => Show (Exp a) where; showsPrec = showsPrec1
 
-data C_Expr a
-  = C_Var a
-  | C_Bound
-  | C_Env !Word64
-  | C_UInt64 !Word64
-  | C_Add (C_Expr a) (C_Expr a)
-  | C_Mult (C_Expr a) (C_Expr a)
-  | C_Lam [C_Expr a] (C_Expr a)
-  | C_App (C_Expr a) (C_Expr a)
-  deriving (Show, Functor, Foldable, Traversable)
-makeBound ''C_Expr
+data Pos = Fst | Snd
+  deriving (Eq, Show)
 
-cc :: Ord a => Expr a -> C_Expr a
-cc = fst . go
+data LL a
+  = LVar a
+  | LUInt64 !Word64
+  | LName String
+  | LProduct [LL a]
+  | LProj !Word64 (LL a)
+  | LPack (LL a) (LL a)
+  | LUnpack (LL a) (Scope Pos LL a)
+  | LApp (LL a) (LL a) (LL a)
+  | LBin Op (LL a) (LL a)
+  deriving (Functor, Foldable, Traversable)
+makeBound ''LL
+deriveEq1 ''LL
+deriveShow1 ''LL
+instance Eq a => Eq (LL a) where; (==) = eq1
+instance Show a => Show (LL a) where; showsPrec = showsPrec1
+
+data LVar = LEnv Int | LArg
+  deriving (Eq, Show)
+
+lvar :: (Int -> r) -> r -> LVar -> r
+lvar f _ (LEnv a) = f a
+lvar _ a LArg = a
+
+data LDef a = LDef { lName :: String, lBody :: Scope LVar LL a }
+  deriving (Eq, Show, Functor, Foldable, Traversable)
+
+freshName :: MonadState [a] m => m a
+freshName = do
+  s <- get
+  case s of
+    s' : ss' -> s' <$ put ss'
+    [] -> undefined
+
+trans :: forall a. Eq a => Exp a -> (LL a, [LDef a])
+trans ex = (val, defs)
   where
-    go :: Ord a => Expr a -> (C_Expr a, [a])
-    go ex =
-      case ex of
-        Var a -> (C_Var a, [a])
-        UInt64 a -> (C_UInt64 a, [])
-        Add a b ->
-          let
-            (a', vs1) = go a
-            (b', vs2) = go b
-          in
-            (C_Add a' b', vs1 `union` vs2)
-        Mult a b ->
-          let
-            (a', vs1) = go a
-            (b', vs2) = go b
-          in
-            (C_Mult a' b', vs1 `union` vs2)
-        App a b ->
-          let
-            (a', vs1) = go a
-            (b', vs2) = go b
-          in
-            (C_App a' b', vs1 `union` vs2)
-        Lam a ->
-          let
-            (a', vs) = go $ fromScope a
-            fvs = foldr (unvar (\_ -> id) (:)) [] vs
-            v_map = Map.fromList $ zip fvs [0..]
-          in
-            ( C_Lam
-                (C_Var <$> fvs)
-                (a' >>=
-                unvar
-                  (\_ -> C_Bound)
-                  (\v -> maybe undefined C_Env $ Map.lookup v v_map))
-            , fvs
-            )
+    ((val, _), defs) = runWriter $ evalStateT (go F ex) (('f' :) . show <$> [0::Int ..])
+
+    go ::
+      forall b m.
+      (Eq b, MonadWriter [LDef a] m, MonadState [String] m, MonadFix m) =>
+      (b -> Var LVar a) ->
+      Exp b -> m (LL b, [b])
+    go _ (Var a) = pure (LVar a, [a])
+    go f (Lam a) = do
+      rec
+        let replace = unvar (\_ -> B LArg) (\b -> maybe (f b) (B . LEnv) $ elemIndex b vs')
+        (a', vs) <- go replace $ fromScope a
+        let vs' = foldr (unvar (const id) (:)) [] vs
+      n <- freshName
+      tell [LDef n $ toScope $ replace <$> a']
+      pure (LPack (LName n) (LProduct $ LVar <$> vs'), vs')
+    go f (App a b) = do
+      (a', vs1) <- go f a
+      (b', vs2) <- go f b
+      pure (LUnpack a' (toScope $ LApp (LVar $ B Fst) (LVar $ B Snd) (F <$> b')), vs1 `union` vs2)
+    go f (Bin o a b) = do
+      (a', vs1) <- go f a
+      (b', vs2) <- go f b
+      pure (LBin o a' b', vs1 `union` vs2)
+    go _ (UInt64 a) = pure (LUInt64 a, [])
