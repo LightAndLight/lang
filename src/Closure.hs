01b6233ee007ef91c793cd28db1cd8d0a41b19de
Isaac Elliott <isaace71295@gmail.com>
2019-07-10 16:36:40 +1000

    there's a mistake

--- src/Closure.hs
+++ src/Closure.hs
@@ -9,8 +9,8 @@ import Bound.Scope.Simple (Scope, toScope)
 import Bound.TH (makeBound)
 import Bound.Var (Var(..), unvar)
 import Control.Monad.Fix (MonadFix)
-import Control.Monad.State (MonadState, evalStateT, get, put)
-import Control.Monad.Writer (MonadWriter, runWriter, tell)
+import Control.Monad.State (MonadState, evalState, get, put)
+import Control.Monad.Writer (MonadWriter, runWriterT, tell)
 import Data.Deriving (deriveEq1, deriveShow1)
 import Data.Functor.Classes (eq1, showsPrec1)
 import Data.List (elemIndex, union)
@@ -58,16 +58,23 @@ freshName = do
     s' : ss' -> s' <$ put ss'
     [] -> undefined
 
-trans :: forall ty tm. Eq tm => Core ty tm -> ([Def tm], Closure tm)
-trans ex = (defs, val)
-  where
-    ((val, _), defs) = runWriter $ evalStateT (go (Var . F) ex) (Text.pack . ('f' :) . show <$> [0::Int ..])
-
+trans ::
+  forall ty tm m.
+  (MonadFix m, MonadState [Text] m, Eq tm) =>
+  Core ty tm -> m ([Def tm], Closure tm)
+trans ex = do
+  ((val, _), defs) <- runWriterT $ go (Var . F) ex
+  pure (defs, val)
+    where
     go ::
-      forall x b m.
-      (Eq b, MonadWriter [Def tm] m, MonadState [Text] m, MonadFix m) =>
+      forall x b n.
+      ( Eq b
+      , MonadWriter [Def tm] n
+      , MonadState [Text] n
+      , MonadFix n
+      ) =>
       (b -> Closure (Var LVar tm)) ->
-      Core x b -> m (Closure b, [b])
+      Core x b -> n (Closure b, [b])
     go _ (Core.Var a) = pure (Var a, [a])
     go f (Core.AppType a _) = go f a
     go f (Core.AbsType _ _ a) = go f $ fromBiscopeL a
@@ -86,23 +93,45 @@ trans ex = (defs, val)
     go f (Core.App a b) = do
       (a', vs1) <- go f a
       (b', vs2) <- go f b
-      pure (Unpack a' (toScope $ App (Var $ B Fst) (Var $ B Snd) (F <$> b')), vs1 `union` vs2)
+      let vs = vs1 `union` vs2
+      n <- freshName
+      let
+        newTm =
+          Unpack a' (toScope $ App (Var $ B Fst) (Var $ B Snd) (F <$> b')) >>=
+          \var ->
+            maybe
+              undefined
+              (\ix -> Proj (fromIntegral ix) (Var $ B LEnv))
+              (elemIndex var vs)
+      tell [GenDef n . toScope $ newTm]
+      pure (Closure (Name n) (Product $ Var <$> vs), vs)
     go f (Core.Bin o a b) = do
       (a', vs1) <- go f a
       (b', vs2) <- go f b
       pure (Bin o a' b', vs1 `union` vs2)
     go _ (Core.UInt64 a) = pure (UInt64 a, [])
 
-transDef :: forall ty tm. Eq tm => Core.Def ty tm -> [Def tm]
-transDef (Core.Def name tm) = UserDef name tm' : ds
-  where
-    (ds, tm') = trans tm
+transDef ::
+  forall ty tm m.
+  (MonadState [Text] m, MonadFix m, Eq tm) =>
+  Core.Def ty tm ->
+  m [Def tm]
+transDef (Core.Def name tm) = do
+  (ds, tm') <- trans tm
+  pure $ UserDef name tm' : ds
 
-transDefs :: Eq tm => [Core.Def ty tm] -> [Def tm]
-transDefs ds = ds >>= transDef
+transDefs ::
+  (MonadState [Text] m, MonadFix m, Eq tm) =>
+  [Core.Def ty tm] ->
+  m [Def tm]
+transDefs [] = pure []
+transDefs (d:ds) = (++) <$> transDef d <*> transDefs ds
 
-transProgram :: Eq tm => ([Core.Def ty tm], Core ty tm) -> ([Def tm], Closure tm)
-transProgram (ds, tm) = (ds2 <> ds1, tm')
-  where
-    (ds1, tm') = trans tm
-    ds2 = transDefs ds
+transProgram ::
+  Eq tm =>
+  ([Core.Def ty tm], Core ty tm) -> ([Def tm], Closure tm)
+transProgram (ds, tm) =
+  flip evalState (Text.pack . ('f' :) . show <$> [0::Int ..]) $ do
+    (ds1, tm') <- trans tm
+    ds2 <- transDefs ds
+    pure (ds2 <> ds1, tm')
